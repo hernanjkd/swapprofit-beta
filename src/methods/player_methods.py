6d6a5f8fdecb133a939d8649df1fe0f221667f35
GGabe <gherndon5@gmail.com>
2020-10-20 15:28:15 -0400

    jge

--- src/methods/player_methods.py
+++ src/methods/player_methods.py
@@ -1086,7 +1086,7 @@ def attach(app):
     @role_jwt_required(['user'])
     def send_message(user_id, their_id, chat_id):
 
-        req = utils.check_params( request.get_json(), 'user_id', 'message' )
+        req = utils.check_params( request.get_json(), 'user_id', 'their_id', 'message' )
 
         # messages have a 100 char limit, make sure to break it up
         print('got past req')
