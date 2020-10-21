8aadedfdd5e5f8df6793a6df2757e590d4c69bf9
GGabe <gherndon5@gmail.com>
2020-10-21 11:45:45 -0400

    ehhh

--- src/models.py
+++ src/models.py
@@ -527,7 +527,7 @@ class Chats(db.Model):
     def serialize2(self):
         the_last_message = [x.serialize() for x in self.messages]
         # y = the_last_message[-1]
-        # print('the_last_message', the_last_message)
+        print('the_last_message', the_last_message)
         # print('y', y)
 
         return {
@@ -537,7 +537,7 @@ class Chats(db.Model):
             'status': self.status._value_,
             'created_at': self.created_at,
             'updated_at': self.updated_at,
-            'last_message': the_last_message
+            'last_message': [the_last_message[-1]]
         }
 
 
