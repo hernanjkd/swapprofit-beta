bcfd50aab1ff562fa50e07be428917c351bdd6b4
GGabe <gherndon5@gmail.com>
2020-10-23 18:18:45 -0400

    try

---
+++ migrations/versions/f6bfa29816c8_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: f6bfa29816c8
+Revises: 
+Create Date: 2020-10-23 18:18:07.589978
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f6bfa29816c8'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
