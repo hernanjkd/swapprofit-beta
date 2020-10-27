f5a55b08bd44b00123e2d12e07e242386d4770cc
GGabe <gherndon5@gmail.com>
2020-10-27 18:15:50 -0400

    debug4

--- src/methods/admin_methods.py
+++ src/methods/admin_methods.py
@@ -85,7 +85,9 @@ def attach(app):
             .filter( m.Tournaments.flights.any(
                 m.Flights.start_at < close_time
             ))
-        ts = db.session.query(m.Tournaments).filter( m.Tournaments.status == 'open')
+        ts = db.session.query(m.Tournaments).filter( m.Tournaments.flights.any(
+                m.Flights.start_at < close_time
+            ))
         for t in ts:
             print('is it t', t)
         for trmnt in trmnts:
