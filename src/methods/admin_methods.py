3dfcf935cf245666933299294fd703e7bbca34be
GGabe <gherndon5@gmail.com>
2021-01-20 12:57:22 -0500

    check now

--- src/methods/admin_methods.py
+++ src/methods/admin_methods.py
@@ -170,7 +170,9 @@ def attach(app):
                         .filter( Buy_ins.flight.has( tournament_id=trmnt.id )) \
                         .filter( m.Buy_ins.user_id==user.id ) \
                         .order_by( m.Buy_ins.id.desc() ).first()
-        
+                    
+                    print('check if this correct', user, user.id)
+
                     if user.event_update is True:
                         
                         # start_time = est.strftime("%b. %d, %a. %I:%M %p")
