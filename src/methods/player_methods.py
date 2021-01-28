8bf0d0197ceec5d26fda589b32dc4e8826547fba
GGabe <gherndon5@gmail.com>
2021-01-28 16:37:38 -0500

    jdnue

--- src/methods/player_methods.py
+++ src/methods/player_methods.py
@@ -253,8 +253,8 @@ def attach(app):
             print('checking for hendon', req['hendon_url'])
             existingHendonURL = Profiles.query.filter_by(hendon_url=req['hendon_url']).first() is not None
             print("User already has this", existingHendonURL)
-            if existingHendonURL is True:
-                raise APIException('This Hendon Mob profile has already been assigned to another user.', 400)
+            if existingHendonURL == True:
+                raise APIException('A profile already exists with "id": ', 400)
         
         utils.update_table(prof, req, ignore=['profile_pic_url','pokersociety_id', 'roi_rating','swap_rating'])
 
