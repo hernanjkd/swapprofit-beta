f6b1e264e217f7fc9ba62a28f07b4c73fdf07dcb
GGabe <gherndon5@gmail.com>
2021-02-04 14:35:24 -0500

    to the promised land

--- src/methods/public_methods.py
+++ src/methods/public_methods.py
@@ -105,7 +105,7 @@ def attach(app):
             'jwt': jwt.encode(identity, os.environ['JWT_SECRET_KEY'], algorithm='HS256')
         }), 200
 
-    @app.route('/redirect')
+    @app.route('/redirect', methods=['GET'])
     def redirectTo():
         return redirect("swapprofit://home")
 
