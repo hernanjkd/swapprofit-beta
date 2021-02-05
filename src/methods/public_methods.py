5da455ae487d0120c1cc19e0b19061dc983ab685
GGabe <gherndon5@gmail.com>
2021-02-05 11:04:02 -0500

    donot return

--- src/methods/public_methods.py
+++ src/methods/public_methods.py
@@ -108,7 +108,7 @@ def attach(app):
     @app.route('/redirect', methods=['GET'])
     def redirectTo():
         print('was redirecyrf')
-        return redirect("swapprofit://home")
+        redirect("swapprofit://home")
 
 
 
